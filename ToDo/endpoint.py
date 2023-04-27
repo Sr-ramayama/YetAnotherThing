e32a55929f00100e897538078f6aebe38b4d6377
Ramayama <prodigytone@gmail.com>
2023-04-27 18:29:52 +0200

    added new route

--- ToDo/endpoint.py
+++ ToDo/endpoint.py
@@ -11,7 +11,13 @@ places = [
 def get_places():
     return jsonify(places)
 
-
+@app.route('/places/eat')
+def get_bares():
+    response = {}
+    for key,value in places.item():
+        if value == 'bar':
+            response.append({key:value})
+    return response
 @app.route('/places', methods=['POST'])
 def add_places():
     places.append(request.get_json())
