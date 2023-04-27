a2fffe160471d9212817476adc01cf8b31c306a5
Ramayama <prodigytone@gmail.com>
2023-04-27 16:24:13 +0200

    Fixing typo

--- ToDo/endpoint.py
+++ ToDo/endpoint.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+places = [
+    { 'Type': 'bar', 'location': 'Calle Cum', 'Style': 'Indian' }
+]
+
+
+@app.route('/places')
+def get_places():
+    return jsonify(places)
+
+
+@app.route('/places', methods=['POST'])
+def add_income():
+    places.append(request.get_json())
+    return '', 204
